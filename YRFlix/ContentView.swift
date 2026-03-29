1bad64774fc3fd75acb1167207586196c61f11ae
rkupadhyayannum <rajiv@annumtechnologies.co>
2026-03-29 23:06:31 +0530

    Intial commit

---
+++ YRFlix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YRFlix
+//
+//  Created by Rajeev  Upadhyay on 29/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
